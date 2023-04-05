decdcbae98e3a0be7c955e587c0700315ebca455
trinhvinhphuc <trinhvinhphuc2001@gmail.com>
2023-04-05 10:27:30 +0700

    Add docs & sendMail func

--- js/custom.js
+++ js/custom.js
@@ -6,3 +6,17 @@ function getYear() {
 }
 
 getYear();
+
+function sendEmail() {
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "trinhvinhphuc2001@gmail.com",
+        Password : "91D51A1E9E9B6C1A71F65EA73E24CFF6ED0C",
+        To : "trinhvinhphuc2001@gmail.com",
+        From : "trinhvinhphuc2001@gmail.com",
+        Subject : document.getElementById("inputSubject4").value,
+        Body : document.getElementById("inputName4").value+" ("+document.getElementById("inputEmail4").value+") has a message:\n"+document.getElementById("inputMessage").value
+    }).then(
+      message => alert(message)
+    );  
+}
